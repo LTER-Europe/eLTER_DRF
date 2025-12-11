9d5a92461699247443c60b441e4ed8d8f274fe4f
Alessandro Oggioni <oggioniale@gmail.com>
2025-12-11 16:51:15 +0100

    Clean up prefixes in RENDERING_UPDATE
    Removed unused prefixes from the RENDERING_UPDATE.

--- src/update.py
+++ src/update.py
@@ -4,17 +4,9 @@ import requests
 FILE_NAME = os.environ['FILE_NAME']
 
 RENDERING_UPDATE = """@prefix xsd: <http://www.w3.org/2001/XMLSchema#> .
-@prefix dct: <http://purl.org/dc/terms/> .
 @prefix owl: <http://www.w3.org/2002/07/owl#> .
 @prefix puv: <https://w3id.org/env/puv#> .
-@prefix qudt: <http://qudt.org/vocab/> .
-@prefix rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> .
 @prefix rdfs: <http://www.w3.org/2000/01/rdf-schema#> .
-@prefix omv: <http://omv.ontoware.org/2005/05/ontology> .
-@prefix schema: <http://schema.org/> .
-@prefix dwc: <http://rs.tdwg.org/dwc/terms/> .
-@prefix unit: <http://qudt.org/vocab/unit/> .
-@prefix skos: <http://www.w3.org/2004/02/skos/core#> .
 
 puv:uom
   a owl:ObjectProperty ;
